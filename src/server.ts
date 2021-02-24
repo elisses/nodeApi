107f114f06536ced33882903417358b79f8da071
Elis <fernandalisses@gmail.com>
2021-02-24 03:00:43 +0100

    criando usuario

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,11 @@
 import 'reflect-metadata'
 import express from 'express';
 import './database';
+import { router } from './router';
 
 const app = express();
 
-
-// http://localhost:3000/users
-app.get("/users", (request, response) => {
-    return response.json({ message: "Hello Viadooooo!" })
-})
-
-// 1 param => Rota(Recurso API)
-// 2 param => request, response
-
-app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-
-    return response.json({ message: "recebeu a mensagem Viadooooo!" })
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running!"));
